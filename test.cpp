2b00985c78cc6aeb0462cffc935853f9de9eef73
wyl <1506295604@qq.com>
2023-11-23 11:42:30 +0800

    add first change

--- test.cpp
+++ test.cpp
@@ -1,11 +1,10 @@
-﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
 #include <iostream>
-
+using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
+    cout << "第一次修改" << endl;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
